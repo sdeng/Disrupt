c61e234d711aa7bbe2b23462b25740d9ce7aa57e
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2014-11-15 15:27:11 -0500

    + encounter viewset

--- src/api/viewsets/__init__.py
+++ src/api/viewsets/__init__.py
@@ -1,6 +1,6 @@
 import re
 from rest_framework_extensions.routers import ExtendedDefaultRouter
-from core.models import *
-
 
 router = ExtendedDefaultRouter(trailing_slash=False)
+
+from src.api.viewsets.encounter import *
